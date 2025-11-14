41457f829b85757217512ec0267af7726c39b4dc
oskwame <nanaqwamee124@gmail.com>
2025-11-14 12:04:08 +0000

    modified Post to use props

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,10 +1,19 @@
+import { PostProps } from '@/interfaces'
 import React from 'react'
 
-const PostsPage: React.FC = () => {
+const PostsPage: React.FC<PostProps> = ({title,body,userId, id}) => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-semibold">Posts</h1>
+  <div className='max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl 
+  transition-shadow duration-300'>
+    <div className='mb-4'>
+      <h2 className='text-2xl font-bold mb-2'>{title}</h2>
+      <p className='text-gray-700'>{body}</p>
     </div>
+    <div className='text-sm text-gray-500'>
+      <p>User ID: {userId}</p>
+      <p>Post ID: {id}</p>
+    </div>
+  </div>
   )
 }
 
